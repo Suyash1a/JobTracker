cbfe5847ded5929342b5782b626cf075540c7060
suyashhagarwal <suyash1606.agarwal@gmail.com>
2026-03-20 16:25:35 +0530

    Comments added in InterviewType.java

--- jobtracker/src/main/java/com/jobtracker/jobtracker/enums/InterviewType.java
+++ jobtracker/src/main/java/com/jobtracker/jobtracker/enums/InterviewType.java
@@ -5,6 +5,7 @@ public enum InterviewType {
     Managerial,
     HR,
     Technical
-
 }
 
+// In this I am keeping a track of interview types, like which all interviews are scheduled or I am about to give.
+// Interview types are DSA round, Managerial round, HR round, Technical round and so on.
