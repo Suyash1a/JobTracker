31537680e4c13ca0fd0cad2f8ae47610058040e3
suyashhagarwal <suyash1606.agarwal@gmail.com>
2026-03-23 23:50:21 +0530

    removed cors from controller, added seperately

--- jobtracker/src/main/java/com/jobtracker/jobtracker/controller/JobController.java
+++ jobtracker/src/main/java/com/jobtracker/jobtracker/controller/JobController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/jobs")
 @RequiredArgsConstructor
